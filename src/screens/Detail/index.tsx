c034feb05686dad07eaac4a1428d910037d91919
cleytonRR <cleytonrodrigues69@live.com>
2020-07-25 23:51:41 -0300

    Create Atribute component

---
+++ src/screens/Detail/index.tsx
@@ -0,0 +1,76 @@
+import React from "react";
+import {
+  StatusBar,
+  ImageBackground,
+  ContainerLinearGradient,
+  ContainerTitle,
+  ContainerScroll,
+  SubTitle,
+  Title,
+  ContainerAtribute,
+  ContainerDetail,
+  AtributeIcone,
+  AtributeData,
+  Description,
+} from "./styles";
+
+import image from "../../assets/chars/spider-man.png";
+import Age from "../../assets/icons/age.svg";
+import Weight from "../../assets/icons/weight.svg";
+import Universe from "../../assets/icons/universe.svg";
+import Height from "../../assets/icons/height.svg";
+
+var atributesIcons = [
+  {
+    label: Age,
+    age: 31,
+  },
+  {
+    label: Weight,
+  },
+  {
+    label: Universe,
+  },
+  {
+    label: Height,
+  },
+];
+
+const Detail = () => {
+  return (
+    <>
+      <StatusBar />
+      <ImageBackground source={image}>
+        <ContainerLinearGradient />
+        <ContainerTitle>
+          <SubTitle>Peter Parker</SubTitle>
+          <Title>Homem Aranha</Title>
+        </ContainerTitle>
+        <ContainerScroll>
+          <ContainerAtribute>
+            {atributesIcons.map((item, key) => (
+              <ContainerDetail key={key}>
+                <item.label key={key} width={50} height={30} />
+                <AtributeData>30 anos</AtributeData>
+              </ContainerDetail>
+            ))}
+          </ContainerAtribute>
+          <Description>
+            Em Forest Hills, Queens, Nova York, o estudante de ensino médio,
+            Peter Parker, é um cientista orfão que vive com seu tio Ben e tia
+            May. Ele é mordido por uma aranha radioativa em uma exposição
+            científica e adquire a agilidade e a força proporcional de um
+            aracnídeo. Junto com a super força, Parker ganha a capacidade de
+            andar nas paredes e tetos. Através de sua habilidade nativa para a
+            ciência, ele desenvolve um aparelho que o permitir lançar teias
+            artificiais. Inicialmente buscando capitalizar suas novas
+            habilidades, Parker cria um traje e, como Homem Aranha, torna-se uma
+            estrela de televisão."
+          </Description>
+        </ContainerScroll>
+      </ImageBackground>
+    </>
+  );
+};
+
+export default Detail;
---
+++ src/screens/Detail/styles.ts
@@ -0,0 +1,66 @@
+import styled from "styled-components/native";
+import { LinearGradient } from "expo-linear-gradient";
+
+export const StatusBar = styled.StatusBar.attrs(() => {
+  barStyle: "dark-content";
+})``;
+
+export const ImageBackground = styled.ImageBackground.attrs(() => {
+  resizeMode: "cover";
+})`
+  flex: 1;
+`;
+
+export const ContainerLinearGradient = styled(LinearGradient).attrs(() => ({
+  colors: ["rgba(0,0,0,0.0)", "#000000"],
+  start: [0.4, -0.1],
+}))`
+  position: absolute;
+  height: 100%;
+  width: 100%;
+`;
+
+export const ContainerTitle = styled.View`
+  flex: 1;
+  justify-content: flex-end;
+  padding: 5%;
+`;
+
+export const SubTitle = styled.Text`
+  font-size: 20px;
+  font-family: "GilroyMedium";
+  color: #b7b7c8;
+`;
+export const Title = styled.Text`
+  color: #f8f8f8;
+  font-family: "GilroyHeavy";
+  font-size: 40px;
+`;
+
+export const ContainerScroll = styled.ScrollView`
+  flex: 1;
+  padding: 5%;
+`;
+export const ContainerAtribute = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  margin-bottom: 20px;
+`;
+
+export const ContainerDetail = styled.View`
+  justify-content: center;
+  align-items: center;
+  padding-top: 25px;
+`;
+export const AtributeIcone = styled.Image``;
+export const AtributeData = styled.Text`
+  font-family: "GilroyMedium";
+  color: #f8f8f8;
+  font-size: 12px;
+  margin-top: 5px;
+`;
+export const Description = styled.Text`
+  font-size: 14px;
+  font-family: "GilroyMedium";
+  color: #b7b7c8;
+`;
