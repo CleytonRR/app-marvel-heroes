fe903b2e52e51cf827698d1328f1330ae04d71a2
cleytonRR <cleytonrodrigues69@live.com>
2020-07-29 20:48:44 -0300

    Receive props in component

--- src/components/SkillSection/index.tsx
+++ src/components/SkillSection/index.tsx
@@ -1,12 +1,24 @@
 import React from "react";
-import { Container, TitleSkill, CustomSlider } from "./styles";
+import { Container, TitleSkill } from "./styles";
 
 import SkillBar from "../SkillBar";
-const SkillSection = () => {
+
+const SkillSection = ({ skills }) => {
+  function createSkillList(skillsData) {
+    const skillNames = Object.keys(skillsData);
+    var skillList = [];
+    skillNames.forEach((item) => {
+      skillList.push({
+        label: item,
+        quantity: `${skillsData[item]}%`,
+      });
+    });
+    return skillList;
+  }
   return (
     <Container>
       <TitleSkill>Habilidade</TitleSkill>
-      <SkillBar />
+      <SkillBar skill={createSkillList(skills)} />
     </Container>
   );
 };
