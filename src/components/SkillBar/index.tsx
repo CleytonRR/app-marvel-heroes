69aaed03f82a0fdfa064c2f5506c09a9c56f1f2a
cleytonRR <cleytonrodrigues69@live.com>
2020-07-29 20:49:04 -0300

    Receive props in component

--- src/components/SkillBar/index.tsx
+++ src/components/SkillBar/index.tsx
@@ -2,62 +2,41 @@ import React from "react";
 import Svg, { Line } from "react-native-svg";
 import { Container, SkillName, ContainerBar } from "./styles";
 
-const SkillBar = () => {
+interface SkillType {
+  [index: number]: { label: string; quantity: int };
+}
+
+const SkillBar = ({ skill }) => {
   return (
     <>
-      <Container>
-        <SkillName>Força</SkillName>
-        <ContainerBar>
-          <Svg height="15" width="100%">
-            <Line
-              x1="0"
-              y1="0"
-              x2="100%"
-              y2="0"
-              stroke="rgba(255,255,255,0.2)"
-              strokeWidth="100%"
-              strokeDasharray="3"
-            />
-            <Line
-              x1="0"
-              y1="0"
-              x2="10"
-              y2="0"
-              clipPath="url(#clip)"
-              stroke="rgba(255,255,255,0.7)"
-              strokeWidth="100%"
-              strokeDasharray="3"
-            />
-          </Svg>
-        </ContainerBar>
-      </Container>
-
-      <Container>
-        <SkillName>Inteligência</SkillName>
-        <ContainerBar>
-          <Svg height="15" width="100%">
-            <Line
-              x1="0"
-              y1="0"
-              x2="100%"
-              y2="0"
-              stroke="rgba(255,255,255,0.2)"
-              strokeWidth="100%"
-              strokeDasharray="3"
-            />
-            <Line
-              x1="0"
-              y1="0"
-              x2="10"
-              y2="0"
-              clipPath="url(#clip)"
-              stroke="rgba(255,255,255,0.7)"
-              strokeWidth="100%"
-              strokeDasharray="3"
-            />
-          </Svg>
-        </ContainerBar>
-      </Container>
+      {skill.map((item, index) => (
+        <Container key={index}>
+          <SkillName>{item.label}</SkillName>
+          <ContainerBar>
+            <Svg height="15" width="100%">
+              <Line
+                x1="0"
+                y1="0"
+                x2="100%"
+                y2="0"
+                stroke="rgba(255,255,255,0.2)"
+                strokeWidth="100%"
+                strokeDasharray="3"
+              />
+              <Line
+                x1="0"
+                y1="0"
+                x2={item.quantity}
+                y2="0"
+                clipPath="url(#clip)"
+                stroke="rgba(255,255,255,0.7)"
+                strokeWidth="100%"
+                strokeDasharray="3"
+              />
+            </Svg>
+          </ContainerBar>
+        </Container>
+      ))}
     </>
   );
 };
